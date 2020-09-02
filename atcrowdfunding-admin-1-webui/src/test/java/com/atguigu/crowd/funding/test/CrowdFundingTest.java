900e8c287ffc2e68b932c5291235d834f8f0258e
samsin-lsx <869301015@qq.com>
2020-09-02 10:39:08 +0800

    进行测试。

--- atcrowdfunding-admin-1-webui/src/test/java/com/atguigu/crowd/funding/test/CrowdFundingTest.java
+++ atcrowdfunding-admin-1-webui/src/test/java/com/atguigu/crowd/funding/test/CrowdFundingTest.java
@@ -15,19 +15,24 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.atguigu.crowd.funding.entity.Admin;
 import com.atguigu.crowd.funding.service.api.AdminService;
 
+/**
+ * 测试类
+ * 
+ * @author lishangxing
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:spring-persist-mybatis.xml", "classpath:spring-persist-tx.xml"})
+@ContextConfiguration(locations = { "classpath:spring-persist-mybatis.xml", "classpath:spring-persist-tx.xml" })
 public class CrowdFundingTest {
 	@Autowired
 	private DataSource dataSource;
 	@Autowired
 	private AdminService adminService;
-	
+
 	@Test
 	public void testTranscation() {
 		this.adminService.updateAdmin();
 	}
-	
+
 	@Test
 	public void testMyBatisGetAll() {
 		List<Admin> adminList = this.adminService.getAll();
@@ -35,7 +40,7 @@ public class CrowdFundingTest {
 			System.out.println(admin);
 		}
 	}
-	
+
 	@Test
 	public void testConnection() throws SQLException {
 		Connection connection = this.dataSource.getConnection();
